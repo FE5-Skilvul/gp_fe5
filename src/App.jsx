21557cbbb067b3548067853263ed87b49ebde1e3
Athif Tafrihan Achyar <85839001+Athifr@users.noreply.github.com>
2022-11-19 21:14:30 +0700

    route register

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/daftar/kampus" element={<RegisterKampus />} />
+        <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/detail" element={<Detail/>} />
       </Routes>
