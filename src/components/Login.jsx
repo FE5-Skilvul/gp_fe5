1b5f880d740005385f75924935c26ed3580b4409
Athif Tafrihan Achyar <85839001+Athifr@users.noreply.github.com>
2022-11-20 12:46:05 +0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { getUser } from "../redux/action/listUserAction";
+
 
 export default function Login() {
+  const [emailUser, setEmailUser] = useState("");
+  const [passwordUser, setPasswordUser] = useState("");
+  const state = useSelector((state) => state.listUser);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  const handlingSubmit = () => {
+    const data = {
+      emailUser,
+      passwordUser,
+    };
+
+    let user = state.list.find((u) => u.email === emailUser);
+
+    if (!user || user.password != passwordUser) {
+      alert("password atau username salah");
+    } else {
+      localStorage.setItem("name", user.firstName);
+      navigate("/detail");
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -20,28 +50,40 @@ export default function Login() {
                     <h1 className="card-title">Masuk</h1>
                     <div className="card-text mt-2">
                       <div className="mb-3">
-                        <label className="form-label">
-                          Email
-                        </label>
+                        <label className="form-label">Email</label>
                         <input
                           type="text"
                           className="form-control"
                           id="email"
                           placeholder="jhondoe@gmail.com"
+                          value={emailUser}
+                          onChange={(e) => setEmailUser(e.target.value)}
                         />
                       </div>
                       <div className="mb-3">
-                        <label className="form-label">
-                          Password
-                        </label>
+                        <label className="form-label">Password</label>
                         <input
-                          type="text"
+                          type="password"
                           className="form-control"
                           id="password"
                           placeholder="Kata Sandi"
+                          value={passwordUser}
+                          onChange={(e) => setPasswordUser(e.target.value)}
                         />
                       </div>
-                      <button className="btn btn-primary col-12">Masuk</button>
+                      <button
+                        className="btn btn-primary col-12"
+                        onClick={handlingSubmit}
+                      >
+                        Masuk
+                      </button>
+                      <p className="text-center text-muted mt-2">
+                        belum punya akun? klik{" "}
+                        <span>
+                          <Link to={"/"}>disini</Link>
+                        </span>
+
+                      </p>
                     </div>
                   </div>
                 </div>
